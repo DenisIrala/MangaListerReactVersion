c37b8ac76656bb7ae304b1ee3f1f39d5b72b68da
skeeper12 <adrian82009@hotmail.com>
2025-01-31 13:08:23 -0500

    plenty of functionality

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,26 @@ function App() {
   const [demography, setDemography]=useState("");
   const [link, setLink]=useState("");
   const [listy, setList]=useState([]);
+  const [filter, setFilter] =useState("ID");
   
+  const [UpdateID, setUpdateID]=useState();
+
+  const [updating,setIsUpdating]=useState(false);
+
+  var list=[];
+
+  const clear=()=>{
+    setName("");
+    setDescription("");
+    setAuthor("");
+    setNoe(0);
+    setDemography("");
+    setLink("");
+    setUpdateID(0);
+    setIsUpdating(false);
+
+  }
+ 
 
   const add=()=>{
     Axios.post("http://localhost:3001/create",{
@@ -31,16 +50,67 @@ function App() {
    
    
   } 
-  const getList=()=>{
-    Axios.get("http://localhost:3001/getList").then((response)=>{
-      setList(response.data);
-
-    })
+  const getList= async ()=>{
+    try {
+      const response = await Axios.get("http://localhost:3001/getList");
+      console.log("Filtering by "+filter);
+      sortList(filter, response.data);
+    } catch (error) {
+      console.error("Error fetching list:", error);
+    }
   }
+
+  const sortList= async (val, response=listy)=>{  
+    setFilter(val);
+    console.log(val);
+    switch(val){
+      case 'ID': setList([...response].sort((a, b) => a.id - b.id)); break;
+      case 'Name': setList([...response].sort((a, b) => a.nombre_manga-b.nombre_manga)); break;
+      case 'Number of Entries' : setList([...response].sort((a, b) => a.nchap - b.nchap)); break;
+      default:
+        console.log("Default filtering");
+        setList([...response].sort((a, b) => a.id - b.id));
+        break;
+
+    }
+
+  }
+
+  const updateList=()=>{
+    Axios.put("http://localhost:3001/updateList",{
+      name:name,
+      description:description,
+      author:author,
+      noe:noe,
+      demography:demography,
+      link:link,
+      id:UpdateID
+    }).then(()=>{
+      getList()
+      setIsUpdating(false)
+      clear() 
+    })}
+
+  const prepareUpdate=(val)=>{
+    setIsUpdating(true);
+    setName(val.nombre_manga);
+    setDescription(val.descripcion);
+    setAuthor(val.autor);
+    setNoe(val.nchap);
+    setDemography(val.demografia);
+    setLink(val.link);
+    setUpdateID(val.id);
+  }
+
+  const prepareDelete=(val)=>{
+    Axios.delete(`http://localhost:3001/delete/${val.id}`).then(()=>{
+      getList()
+    })}
+  
 
   useEffect(() => {
     getList();
-  }
+  },[]
   )
 //  getList();
 
@@ -56,40 +126,55 @@ function App() {
   <div className="card-header">
     Mangas
   </div>
+  
   <div className="card-body">
     <div className="input-group mb-3">
       <span className="input-group-text" id="basic-addon1">Name</span>
-      <input type="text" onChange={(event)=>{setName(event.target.value);}} className="form-control" placeholder="Naruto" aria-label="Name" aria-describedby="basic-addon1"/>
+      <input type="text" value={name} onChange={(event)=>{setName(event.target.value);}} className="form-control" placeholder="Naruto" aria-label="Name" aria-describedby="basic-addon1"/>
     </div>        
     
     <div className="input-group mb-3">
       <span className="input-group-text" id="basic-addon1">Description</span>
-      <input type="text" onChange={(event)=>{setDescription(event.target.value);}} className="form-control" placeholder="Description" aria-label="Description" aria-describedby="basic-addon1"/>
+      <input type="text" value={description} onChange={(event)=>{setDescription(event.target.value);}} className="form-control" placeholder="Description" aria-label="Description" aria-describedby="basic-addon1"/>
     </div>   
     
     <div className="input-group mb-3">
       <span className="input-group-text" id="basic-addon1">Author</span>
-      <input type="text" onChange={(event)=>{setAuthor(event.target.value);}} className="form-control" placeholder="Masashi Kishimoto" aria-label="Author" aria-describedby="basic-addon1"/>
+      <input type="text" value={author} onChange={(event)=>{setAuthor(event.target.value);}} className="form-control" placeholder="Masashi Kishimoto" aria-label="Author" aria-describedby="basic-addon1"/>
     </div>   
 
     <div className="input-group mb-3">
       <span className="input-group-text" id="basic-addon1">Number of Entries</span>
-      <input type="number" onChange={(event)=>{setNoe(event.target.value);}} className="form-control" placeholder="700" aria-label="NOE" aria-describedby="basic-addon1"/>
+      <input type="number" value={noe} onChange={(event)=>{setNoe(event.target.value);}} className="form-control" placeholder="700" aria-label="NOE" aria-describedby="basic-addon1"/>
     </div>   
 
     <div className="input-group mb-3">
       <span className="input-group-text" id="basic-addon1">Demography</span>
-      <input type="text" onChange={(event)=>{setDemography(event.target.value);}} className="form-control" placeholder="Shonen" aria-label="Demography" aria-describedby="basic-addon1"/>
+      <input type="text" value={demography} onChange={(event)=>{setDemography(event.target.value);}} className="form-control" placeholder="Shonen" aria-label="Demography" aria-describedby="basic-addon1"/>
     </div>   
 
     <div className="input-group mb-3">
       <span className="input-group-text" id="basic-addon1">Link</span>
-      <input type="text" onChange={(event)=>{setLink(event.target.value);}} className="form-control" placeholder="https://en.wikipedia.org/wiki/Naruto" aria-label="Link" aria-describedby="basic-addon1"/>
+      <input type="text" value={link} onChange={(event)=>{setLink(event.target.value);}} className="form-control" placeholder="https://en.wikipedia.org/wiki/Naruto" aria-label="Link" aria-describedby="basic-addon1"/>
     </div>   
         
-    <p className="card-text"><button className="btn btn-success" onClick={add}>Add</button></p>
-    <a href="#" onClick={getList} className="btn btn-primary">Update</a>
+    
+    {
+          updating?<div>
+          <a href="#" onClick={updateList} className="btn btn-primary">Update</a> <a href="#" onClick={clear} className="btn btn-danger">Cancel</a></div>
+          :<p className="card-text"><button className="btn btn-success" onClick={add}>Add</button></p>
+    }
   </div>
+</div>
+
+<div className="filterBox">
+<select id="filter" value={filter} onChange={(event)=>{sortList(event.target.value);}}>
+        <option value="all">Sort By</option>
+        <option value="ID">ID</option>
+        <option value="Name">Name</option>
+        <option value="Number of Entries">Number of Entries</option>
+        {/* Add more options as needed */}
+      </select>
 </div>
 
 <table className="table table-hover">
@@ -108,7 +193,15 @@ function App() {
           <th scope="row">{val.id}</th>
           <td>{val.nombre_manga}</td>
           <td>{val.autor}</td>
-        < td>{val.nchap}</td>
+          <td>{val.nchap}</td>
+          <td>
+          <button onClick={()=>{
+            prepareUpdate(val)
+          }} className="btn btn-info">Update</button>
+          <button onClick={()=>{
+            prepareDelete(val)
+          }} className="btn btn-danger">Delete</button>
+          </td>
         </tr>
       })
     }
--- server/index.js
+++ server/index.js
@@ -12,6 +12,12 @@ const client=new Client({
 })
   */
 
+const corsOptions = {
+  origin: "http://localhost:8081",
+  methods: ["POST", "GET"],
+  credentials: true,
+}
+
 const client=new Client({
   host:"localhost",
   database: "dbpweb",
@@ -60,11 +66,40 @@ client
           console.error('Error executing query', err);
         } else {
       //   console.log('Query result:', result.rows);
-          res.send(result.rows);
+        res.setHeader('Content-Type', 'application/json');
+        res.json(result.rows);
         }
       })
     });
 
+    app.put("/updateList",(req,res)=>{
+      const name= req.body.name;
+      const description=req.body.description;
+      const author=req.body.author;
+      const noe=req.body.noe;
+      const demography=req.body.demography;
+      const link=req.body.link;
+      const updateId=req.body.id;
+
+      client.query('UPDATE "Manga" SET "nombre_manga"=$1, "descripcion"=$2, "link"=$3, "autor"=$4, "nchap"=$5, "demografia"=$6 WHERE "id"=$7;',[name,description,link,author,noe,demography,updateId], (err, result) => {
+        if (err) {
+          console.error('Error executing query', err);
+        } else {
+          res.send("Success!");
+        }
+      });
+    })
+
+    app.delete("/delete/:id",(req,res)=>{
+      const {id}= req.params;
+      client.query('DELETE FROM "Manga" WHERE id=$1;',[id], (err, result) => {
+        if (err) {
+          console.error('Error executing query', err);
+        } else {
+          res.send("Success!");
+        }
+      });
+    })
 
 	})
 	.catch((err) => {
@@ -76,4 +111,8 @@ client
 
 app.listen(3001,()=>{
   console.log("Server is up.");
+})
+
+app.listen(8080,()=>{
+  console.log("Server is up. 8080");
 })
